0e7dc47ef5c003c87a8809507e98c55fcb817030
FredKy <pinb4llwiz@protonmail.com>
2023-04-14 20:54:39 +0200

    Added two new cards

--- assets/cards/my_cards_database.gd
+++ assets/cards/my_cards_database.gd
@@ -1,31 +1,13 @@
 # Unitinfo = [Type, Attack, Retaliation, Health, Cost, Name, Melee or Ranged, Special Text]
 # Eventinfo = [Type, Cost, Effect]
 # Spellinfo = [Type, Cost, Name, Effect]
-enum {FOOTMAN, ARCHER, SQUAD_LEADER,
-WARRIOR, GUARDIAN, KNIGHT, MERCENARY,
-SPEARMAN, MENTOR, TREBUCHET, ICE_CANNON}
+enum {ICE_CANNON, WARP_TIME, COLD_TOUCH}
 
 const DATA = {
+	COLD_TOUCH:
+		["spell", 1, "Cold Touch", "Apply 2 chill\nto an enemy", Rect2(9728,64,256,192)],
 	ICE_CANNON :
-		["spell", 2, "ice_cannon", "Deal 7 damage\nto a unit", Rect2(4864,0,256,192)],
-	FOOTMAN : 
-		["units", 1, 1, 2, 1, "footman", "Melee"],
-	ARCHER :
-		["units", 2, 1, 3, 2, "archer", "Ranged,\nimmune to\nretaliation"],
-	SQUAD_LEADER :
-		["units", 2, 2, 3, 3, "squad_leader", "Melee,\ngive all friendly\n+1 attack and \nretaliation"],
-	WARRIOR :
-		["units", 4, 0, 2, 3, "rogue", "Melee,\nimmune to\nretaliation"],
-	GUARDIAN :
-		["units", 1, 3, 6, 3, "guardian", "Melee,\nprotector - stops the unit\nbehind it\nbeing attacked"],
-	KNIGHT : 
-		["units", 2, 3, 6, 4, "knight", "Melee"],
-	MERCENARY :
-		["units", 2, 2, 0, 2, "mercenary", "Melee,\nalways retaliates\nreturn to supply when damaged\nor at start of next turn\nafter played, increase\ncost by 1"],
-	SPEARMAN :
-		["units", 2, 2, 5, 3, "spearman", "Melee or ranged"],
-	MENTOR :
-		["units", 3, 0, 1, 2, "mentor", "Melee,\nwhen played give\nfriendly unit +2 attack\nand retaliation"],
-	TREBUCHET :
-		["events", 4, "Deal 6 damage\nto a unit"],
+		["spell", 2, "Ice Cannon", "Deal 7 damage\nto an enemy", Rect2(4864,0,256,192)],
+	WARP_TIME:
+		["spell", 3, "Warp Time", "Target enemy passes\n a turn", Rect2(7424,64,256,192)],
 	}
--- cards/my_card_base.gd
+++ cards/my_card_base.gd
@@ -3,11 +3,17 @@ extends MarginContainer
 
 # Declare member variables here.
 @onready var card_database = preload("res://assets/cards/my_cards_database.gd").new()
-var card_name = 'ice_cannon'
+#var card_name = 'ice_cannon'
+#var card_name = 'warp_time'
+var card_name = 'cold_touch'
 @onready var card_info = card_database.DATA[card_database.get(card_name.to_upper())]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print(card_info)
-	$Image.region_rect = card_info[4]
-	print($Image.region_rect)
+	$HiddenPanel/Image.region_rect = card_info[4]
+	print($HiddenPanel/Image.region_rect)
+	$CostRect/CostRect2/Cost.text = str(card_info[1])
+	$VBoxContainer/Name.text = card_info[2]
+	$VBoxContainer/Info.text = card_info[3]
+	
