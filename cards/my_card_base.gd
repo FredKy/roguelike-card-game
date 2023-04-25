2bdaf2c37c66a17a46e039898c419e4b4639da1a
FredKy <pinb4llwiz@protonmail.com>
2023-04-26 00:18:53 +0200

    Fixed zoomed card placement

--- cards/my_card_base.gd
+++ cards/my_card_base.gd
@@ -291,7 +291,8 @@ func _on_focus_mouse_entered():
 			print(get_viewport().size)
 			print(float(get_viewport().size.y)/648)
 			print($'../../../'.CARD_SIZE)
-			targetpos.y = get_viewport().size.y - $'../../../'.CARD_SIZE.y*(float(get_viewport().size.x)/1152)*zoom_scale
+			#targetpos.y = get_viewport().size.y - $'../../../'.CARD_SIZE.y*(float(get_viewport().size.x)/1152)*zoom_scale
+			targetpos.y = 690 - $'../../../'.CARD_SIZE.y*zoom_scale
 			print(targetpos.y)
 			zooming_in = true
 			state = FOCUS_IN_HAND
