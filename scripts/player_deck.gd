0bba97fea3cb6d2f73af58ed5104f5766efe84e9
FredKy <pinb4llwiz@protonmail.com>
2023-05-12 10:15:43 +0200

    Removed redundant script file

--- scripts/player_deck.gd
+++
@@ -1,6 +0,0 @@
-#extends Node
-#var card_list = ["ice_cannon", "ice_cannon", "warp_time", "cold_touch", "cold_touch", "ice_cannon"]
-#@onready var game_state = get_node("/root/GameState")
-var card_list = ["ice_cannon", "ice_cannon", "energy_shield", "energy_shield", "energy_shield", "ice_cannon"]
-#card_list = game_state.global_player_deck
-#var discarded_cards = []
