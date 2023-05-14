06d974352afa11a83befa892b448d54b190c0aaa
FredKy <pinb4llwiz@protonmail.com>
2023-05-14 09:24:29 +0200

    Removed redundant check for real now

--- end_turn_button.gd
+++ end_turn_button.gd
@@ -10,6 +10,5 @@ func _on_gui_input(_event):
 	var player_animation = $'../../Wanderer/VBoxContainer/ImageContainer/AnimatedSprite2D'.animation
 	if Input.is_action_just_released("leftclick") and disabled == false\
 	and player_animation == "idle" and $'../../'.some_enemy_is_alive():
-	#and $'../../Wanderer'.number_of_buffered_attacks == 0:
 		#$'../../'.move_cards_from_hand_to_discard(0.5) #Parameter is how long discard animation takes.
 		$'../../'.end_player_turn()
