212a91b377ae16937fc79e874428d452c596face
FredKy <pinb4llwiz@protonmail.com>
2023-05-14 09:22:07 +0200

    Removed redundant check from end turn button function

--- end_turn_button.gd
+++ end_turn_button.gd
@@ -9,7 +9,7 @@ func _ready():
 func _on_gui_input(_event):
 	var player_animation = $'../../Wanderer/VBoxContainer/ImageContainer/AnimatedSprite2D'.animation
 	if Input.is_action_just_released("leftclick") and disabled == false\
-	and player_animation == "idle" and $'../../'.some_enemy_is_alive()\
-	and $'../../Wanderer'.number_of_buffered_attacks == 0:
+	and player_animation == "idle" and $'../../'.some_enemy_is_alive():
+	#and $'../../Wanderer'.number_of_buffered_attacks == 0:
 		#$'../../'.move_cards_from_hand_to_discard(0.5) #Parameter is how long discard animation takes.
 		$'../../'.end_player_turn()
