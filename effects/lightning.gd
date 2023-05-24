208fecd2c16f7d2804ebdaf6e9d00fcf9aa3a85b
FredKy <pinb4llwiz@protonmail.com>
2023-05-24 23:39:21 +0200

    Minor stuff

--- effects/lightning.gd
+++ effects/lightning.gd
@@ -13,7 +13,7 @@ func init(start, end):
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	await get_tree().create_timer(0.3).timeout
-	visible = false
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
